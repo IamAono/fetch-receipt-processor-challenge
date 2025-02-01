2c1dfc295be2417315da9f40de845bf85d63241c
S.L <43483411+IamAono@users.noreply.github.com>
2025-02-01 11:08:23 -0800

    added endpoint for getting points

--- src/main.go
+++ src/main.go
@@ -2,12 +2,17 @@ package main
 
 import (
 	"log"
+	"net/http"
+	"strconv"
 
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
+	id := 1
+	// a map where the key is the id and the value is the amount of points earned
+	points := make(map[int]int)
 	router := gin.Default()
 	router.Use(cors.Default())
 
@@ -21,7 +26,26 @@ func main() {
 			return
 		}
 
-		log.Println(receipt)
+		// calculate the amount of points that were earned from the recipt and save the value in a map
+		points[id] = receipt.calcPoints()
+		log.Println("Total points:", points[id])
+
+		c.JSON(http.StatusOK, gin.H{"id": id})
+		id++
+	})
+	router.GET("/receipts/:id/points", func(c *gin.Context) {
+		idReq, err := strconv.Atoi(c.Params.ByName("id"))
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid id"})
+			return
+		}
+		// check if the id being requested exists in the map
+		_, ok := points[idReq]
+		if !ok {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid id"})
+		}
+		c.JSON(http.StatusOK, gin.H{"points": points[idReq]})
+
 	})
 
 	// Start the webservice
